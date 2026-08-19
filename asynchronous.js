98d8687b9c0142a4ecaa396f3ad46cf3408d9e97
codelearner26 <b24cs264@kitsw.ac.in>
2026-08-19 16:07:44 +0530

    Add files via upload

---
+++ asynchronous.js
@@ -0,0 +1,8 @@
+console.log("program started");
+setTimeout(()=>{
+    console.log("Task 1 completed");
+},2000);
+setTimeout(()=>{
+    console.log("Task2 completed");
+},1000);
+console.log("Program ended");
