c16d2632d867ee63199c6226d1727b88c698882b
codelearner26 <b24cs264@kitsw.ac.in>
2026-08-19 16:08:58 +0530

    Add files via upload

---
+++ stream.js
@@ -0,0 +1,20 @@
+const fs=require("fs");
+const readStream=fs.createReadStream("input.txt",{
+    encoding:"utf8"
+});
+const writeStream=fs.createWriteStream("output.txt");
+readStream.on("data",(chunk)=>{
+    console.log("Reading chunk:");
+    console.log(chunk);
+    writeStream.write(chunk);
+    });
+readStream.on("end",()=>{
+    console.log("File reading completed");
+    writeStream.end();
+});
+readStream.on("error",(err)=>{
+    console.log("error:",err.message);
+});
+writeStream.on("finish",()=>{
+    console.log("data written successfully");
+});
